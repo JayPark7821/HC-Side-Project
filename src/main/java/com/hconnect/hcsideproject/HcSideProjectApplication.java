315d9ea34a95daf9234d6506a6ab08c2fff39baf
Parkchanyung <50781066+parkchanyung@users.noreply.github.com>
2022-05-18 19:44:43 +0900

    project init

---
+++ src/main/java/com/hconnect/hcsideproject/HcSideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hconnect.hcsideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcSideProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcSideProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hconnect/hcsideproject/HcSideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hconnect.hcsideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcSideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
