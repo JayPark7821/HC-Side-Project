290f2295757fdb454b0c0c77e65c58ba52e3a357
김병조 <belongs2jo@gmail.com>
2022-05-18 20:54:43 +0900

    git commit test

--- src/main/java/com/hconnect/hcsideproject/HcSideProjectApplication.java
+++ src/main/java/com/hconnect/hcsideproject/HcSideProjectApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HcSideProjectApplication {
+	String id="";
+	public static void main(String[] args) {
+		
+		SpringApplication.run(HcSideProjectApplication.class, args);
 
-	public static void main(String[] args) {SpringApplication.run(HcSideProjectApplication.class, args);
 	}
 
 }
